716ac488243bd628ba31be629d16284b837b3d02
Alex <alexa@nicolor.tech>
2021-03-07 20:38:33 -0500

    Initial commit

---
+++ lib/login.dart
@@ -0,0 +1,92 @@
+import 'package:flutter/material.dart';
+import 'package:aquasafe20xx/api.dart' as api;
+import 'package:shared_preferences/shared_preferences.dart';
+
+class LoginPage extends StatefulWidget {
+  @override
+  _LoginPageState createState() => _LoginPageState();
+}
+
+class _LoginPageState extends State<LoginPage> {
+  // Values for the text fields
+  final usernameController = TextEditingController();
+  final passwordController = TextEditingController();
+  final GlobalKey<ScaffoldState> _LoginScaffold = new GlobalKey<ScaffoldState>();
+
+
+  // Widget to display
+  @override
+  Widget build(BuildContext context) {
+    final inputUsername = Padding(
+      padding: EdgeInsets.only(bottom: 10),
+      child: TextField(
+        autocorrect: false,
+        keyboardType: TextInputType.text,
+        decoration: InputDecoration(hintText: 'Username'),
+        controller: usernameController,
+      ),
+    );
+
+    final inputPasswd = Padding(
+      padding: EdgeInsets.only(bottom: 10),
+      child: TextField(
+        autocorrect: false,
+        obscureText: true,
+        keyboardType: TextInputType.text,
+        decoration: InputDecoration(hintText: 'Password'),
+        controller: passwordController,
+      ),
+    );
+
+    Map<String, dynamic> apiResponse;
+    final loginButton = Padding(
+      padding: EdgeInsets.only(bottom: 5),
+      child: ButtonTheme(
+        height: 56,
+        child: ElevatedButton(
+            child: Text(
+              'Login',
+              style: TextStyle(fontSize: 20),
+            ),
+            onPressed: () async => {
+                  // Grabs token using username and password from text fields
+                  apiResponse = await api.API.loginPOST(
+                      usernameController.text, passwordController.text),
+                  // Debug statment REMOVE before production
+                  print(apiResponse.toString()),
+                  String message = "",
+                    {_writeToken(apiResponse["token"])}
+                  else
+                    {
+                      if (apiResponse["msg"].substring(0,3) == "403"){},
+                      ScaffoldMessenger.of(context).showSnackBar(
+                        content: const Text()
+                      )
+                    }
+                }),
+      ),
+    );
+
+    
+    return SafeArea(
+        child: Scaffold(
+          key: _LoginScaffold,
+      body: Center(
+        child: ListView(
+          shrinkWrap: true,
+          padding: EdgeInsets.symmetric(horizontal: 20),
+          children: <Widget>[
+            inputUsername,
+            inputPasswd,
+            loginButton,
+          ],
+        ),
+      ),
+    ));
+  }
+
+  _writeToken(String token) async {
+    final prefs = await SharedPreferences.getInstance();
+    prefs.setString("token", token);
+  }
+}
